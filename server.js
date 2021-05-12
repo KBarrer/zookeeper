03bb2970e5f435d800cc00f165614cb063b7e3ba
KSB <kyle.barrer@gmail.com>
2021-05-12 13:59:25 -0600

    commit for ask BCS

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 const { animals } = require('./data/animals');
 
 
@@ -128,6 +130,11 @@ app.post('/api/animals', (req, res) => {
 
     res.json(req.body);
     }
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
